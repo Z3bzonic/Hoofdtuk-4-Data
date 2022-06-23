3b990c858ae653c5ad76053c4999221493682e06
Z3bzonic <107925003+Z3bzonic@users.noreply.github.com>
2022-06-23 14:15:47 +0200

    Add files via upload

---
+++ Geometric/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Geef een hoek in graden° (enkel het cijfer)");
+int input = Convert.ToInt32(Console.ReadLine());
+double rad = Math.PI * input / 180;
+Console.WriteLine(rad);
+Console.WriteLine(Math.Sin(rad));
+Console.WriteLine(Math.Cos(rad));
+Console.WriteLine(Math.Tan(rad));
---
+++ Supercomputer/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Geef kommagetal één op");
+double get1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Geef kommagetal twee op");
+double get2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Geef kommagetal drie op");
+double get3 = Convert.ToDouble(Console.ReadLine());
+
+
+double getgem = (get1 + get2 + get3) / 3;
+Console.WriteLine(getgem);
